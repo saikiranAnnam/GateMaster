2681a3c4a2466d1d47217c958319dc786a3aef3a
saikiranAnnam <saikiranannam23@gmail.com>
2024-03-21 08:26:30 +0530

    added roles schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  primaryKey,
+  uniqueIndex,
+  text,
+} from "drizzle-orm/pg-core";
 
 // HR application  or shopify store or cloud resource schema
 export const applications = pgTable("applications", {
@@ -7,3 +15,42 @@ export const applications = pgTable("applications", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+// creating user schema
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").defaultRandom().notNull(),
+    email: varchar("email", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    applicationId: uuid("applicationId").references(() => applications.id),
+    password: varchar("password", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      cpk: primaryKey(users.email, users.applicationId), // composite key 
+      idIndex: uniqueIndex("users_id_index").on(users.id),
+    };
+  }
+);
+
+// creating roles
+export const roles = pgTable(
+    "roles",
+    {
+      id: uuid("id").defaultRandom().notNull(),
+      name: varchar("name", { length: 256 }).notNull(),
+      applicationId: uuid("applicationId").references(() => applications.id),
+      permissions: text("permissions").array().$type<Array<string>>(),
+      createdAt: timestamp("created_at").defaultNow().notNull(),
+      updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    },
+    (roles) => {
+      return {
+        cpk: primaryKey(roles.name, roles.applicationId),
+        idIndex: uniqueIndex("roles_id_index").on(roles.id),
+      };
+    }
+  );
