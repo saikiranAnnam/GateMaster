d523231d9417ccf609f45d137d28a5dc4944dc3b
saikiranAnnam <saikiranannam23@gmail.com>
2024-03-23 15:41:54 +0530

    added default roles for application

--- src/modules/applications/applications.controllers.ts
+++ src/modules/applications/applications.controllers.ts
@@ -1,4 +1,10 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import {
+  ALL_PERMISSIONS,
+  SYSTEM_ROLES,
+  USER_ROLE_PERMISSIONS,
+} from "../../config/permissions";
+import { createRole } from "../roles/roles.services";
 import { createApplicationBody } from "./applications.schemas";
 import { createApplication } from "./applications.services";
 
@@ -10,7 +16,34 @@ export async function createApplicationHandler(
 
   const application = await createApplication({ name });
 
+  const superAdminRolePromise = createRole({
+    applicationId: application.id,
+    name: SYSTEM_ROLES.SUPER_ADMIN,
+    permissions: ALL_PERMISSIONS as unknown as Array<string>,
+  });
+
+  const applicationUserRolePromise = createRole({
+    applicationId: application.id,
+    name: SYSTEM_ROLES.APPLICATION_USER,
+    permissions: USER_ROLE_PERMISSIONS as unknown as Array<string>,
+  });
+
+  const [superAdminRole, applicationUserRole] = await Promise.allSettled([
+    superAdminRolePromise,
+    applicationUserRolePromise,
+  ]);
+
+  if (superAdminRole.status === "rejected") {
+    throw new Error("Error creating super admin role");
+  }
+
+  if (applicationUserRole.status === "rejected") {
+    throw new Error("Error creating application user role");
+  }
+
   return {
     application,
+    superAdminRole: superAdminRole.value,
+    applicationUserRole: applicationUserRole.value,
   };
 }
